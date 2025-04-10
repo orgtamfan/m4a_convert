dbcfedea3729a9665f68da002cc2b98486f9645f
Olan <110212687+orgtamfan@users.noreply.github.com>
2025-04-10 21:40:02 +0700

    update_converter.py

--- converter.py
+++ converter.py
@@ -3,29 +3,30 @@ from pytubefix.cli import on_progress
 from pydub import AudioSegment
 import os
 
-url = "Your URL Video"
+url = "https://youtu.be/DzLv6W4aYlA?si=_KfLhWuMaoNE79_h"
+directory = "C:/Users/A S U S/OneDrive/Music/Playlists" 
 
 yt = YouTube(url, on_progress_callback=on_progress)
 print(yt.title)
 
 # Downloading the audio from the url
 ys = yt.streams.get_audio_only()
-downloaded_file_path = ys.download()
+file_path = ys.download()
 
-def convert_m4a_to_mp3(input_file):
-    # Load the m4a file from download
+def convert_mp4_to_mp3(input_file, output_dir):
+    # Load the m4a file
     audio = AudioSegment.from_file(input_file, format='m4a')
     
-    # Create the output file by change the extension
-    output_file = input_file.rsplit('.', 1)[0] + '.mp3'
+    # Create the output file name by replacing the extension and adding the output directory
+    output = os.path.join(output_dir, os.path.basename(input_file).rsplit('.', 1)[0] + '.mp3')
     
-    # Export file as mp3
-    audio.export(output_file, format='mp3')
-    print(f"Converted '{input_file}' to '{output_file}'")
+    # Export as mp3
+    audio.export(output, format='mp3')
+    print(f"Converted '{input_file}' to '{output}'")
     
-    # Deleting the original m4a file
+    # Delete the original m4a file
     os.remove(input_file)
     print(f"Deleted the original file: '{input_file}'")
 
-# Convert the downloaded file to MP3
-convert_m4a_to_mp3(downloaded_file_path)
+# Convert the downloaded file to MP3 and specify the output directory
+convert_mp4_to_mp3(file_path, directory)
